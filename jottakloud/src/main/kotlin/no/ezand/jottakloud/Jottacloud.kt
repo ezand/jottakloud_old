c285d37b329f442fea6c11b9b2d9c4c29c9e9f54
Eirik Stenersen Sand <eiriksand@gmail.com>
2017-01-24 23:16:35 +0100

    Refactoring some code

--- jottakloud/src/main/kotlin/no/ezand/jottakloud/Jottacloud.kt
+++ jottakloud/src/main/kotlin/no/ezand/jottakloud/Jottacloud.kt
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import com.fasterxml.jackson.datatype.joda.JodaModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import khttp.get
+import khttp.responses.Response
 import khttp.structures.authorization.BasicAuthorization
 import mu.KLogging
 import no.ezand.jottakloud.data.*
@@ -16,7 +17,6 @@ import java.io.InputStream
 import java.net.URL
 import java.net.URLEncoder
 
-// TODO handle notfound etc. and error handling in general
 class Jottacloud(val baseUrl: URL, authorization: JottacloudAuthorization) {
     companion object : KLogging()
 
@@ -30,56 +30,72 @@ class Jottacloud(val baseUrl: URL, authorization: JottacloudAuthorization) {
     private val user = authorization.username
     private val basicAuthorization = BasicAuthorization(authorization.username, authorization.password)
 
-    fun getUser(): User {
+    fun getUser(): User? {
         val url = buildUrl(user)
         logger.debug { "Getting user: $url" }
 
-        val xml = get(url, auth = basicAuthorization).text
-        return xmlMapper.readValue(xml, User::class.java)
+        val response = get(url, auth = basicAuthorization)
+        return objectOrNull(response)
     }
 
-    fun getDevice(deviceName: String): Device {
+    fun getDevice(deviceName: String): Device? {
         val url = buildUrl("$user/$deviceName")
         logger.debug { "Getting device '$deviceName': $url" }
 
-        val xml = get(url, auth = basicAuthorization).text
-        return xmlMapper.readValue(xml, Device::class.java)
+        val response = get(url, auth = basicAuthorization)
+        return objectOrNull(response)
     }
 
-    fun getMountPoint(deviceName: String, mountPointName: String): MountPoint {
+    fun getMountPoint(deviceName: String, mountPointName: String): MountPoint? {
         val url = buildUrl("$user/$deviceName/$mountPointName")
         logger.debug { "Getting mount point: '$deviceName' -> '$mountPointName': $url" }
 
-        val xml = get(url, auth = basicAuthorization).text
-        return xmlMapper.readValue(xml, MountPoint::class.java)
+        val response = get(url, auth = basicAuthorization)
+        return objectOrNull(response)
     }
 
-    fun getFolder(deviceName: String, mountPointName: String, path: String): FolderDetails {
+    fun getFolder(deviceName: String, mountPointName: String, path: String): FolderDetails? {
         val url = buildUrl("$user/$deviceName/$mountPointName/$path")
         logger.debug { "Getting folder: '$deviceName' -> '$mountPointName' -> '$path': $url" }
 
-        val xml = get(url, auth = basicAuthorization).text
-        return xmlMapper.readValue(xml, FolderDetails::class.java)
+        val response = get(url, auth = basicAuthorization)
+        return objectOrNull(response)
     }
 
-    fun getFile(deviceName: String, mountPointName: String, path: String): FileDetails {
+    fun getFile(deviceName: String, mountPointName: String, path: String): FileDetails? {
         val url = buildUrl("$user/$deviceName/$mountPointName/$path")
         logger.debug { "Getting file: '$deviceName' -> '$mountPointName' -> '$path': $url" }
 
-        val xml = get(url, auth = basicAuthorization).text
-        return xmlMapper.readValue(xml, FileDetails::class.java)
+        val response = get(url, auth = basicAuthorization)
+        return objectOrNull(response)
     }
 
-    fun downloadFile(deviceName: String, mountPointName: String, path: String): InputStream {
+    fun downloadFile(deviceName: String, mountPointName: String, path: String): InputStream? {
         val url = buildUrl("$user/$deviceName/$mountPointName/$path?mode=bin")
         logger.debug { "Getting file: '$deviceName' -> '$mountPointName' -> '$path': $url" }
 
-        return get(url, auth = basicAuthorization, stream = true).raw
+        val response = get(url, auth = basicAuthorization)
+        return streamOrNull(response)
     }
 
     private fun buildUrl(url: String): String {
-        val encodedUrl = url.split("/").map { URLEncoder.encode(it, "utf-8") }.joinToString("/")
+        val encodedUrl = url.split("/")
+                .map { URLEncoder.encode(it, "utf-8") }
+                .joinToString("/")
 
         return "$baseUrl/$encodedUrl"
+    }
+
+    inline private fun <reified T : Any> objectOrNull(response: Response): T? {
+        if (response.statusCode == 200)
+            return xmlMapper.readValue(response.text, T::class.java)
+        else {
+            logger.debug("Got status code ${response.statusCode}: ${response.text}")
+            return null
+        }
+    }
+
+    private fun streamOrNull(response: Response): InputStream? {
+        if (response.statusCode == 200) return response.raw else return null
     }
 }
